1c9652d132121cef9870ac46761da6ad50674a9b
dbollinger <dhbollinger@gmail.com>
2014-04-13 18:43:37 -0500

    denon
    volume buttons

--- pb/amd/pages/denon.js
+++ pb/amd/pages/denon.js
@@ -8,43 +8,55 @@ define([
 	
 	return BaseView.extend({
 
+
 		initView: function () {
-			this.updateVolume();
-			
-		    $('#setVolume').click(function (evt) {
-				$.ajax({
-					url: '/denon/volume',
-					type: 'put',
-					data: JSON.stringify({ value: $('#volumeRange').val() })
-				})
-			});
-			
-			$('#setInput').click(function (evt) {
-				$.ajax({
-					url: '/denon/input',
-					type: 'put',
-					data: JSON.stringify({ value: $('#selectedInput').val() })
-				})
-			});
+		
 		},
 
 		events: {
 			"change #volumeRange": 'updateVolume',
-			"change #selectedInput": 'updateInput'
+			"change #selectedInput": 'updateInput',
+			"click #btnVolumeUp": 'increaseVolume',
+			"click #btnVolumeDown": 'decreaseVolume'
 		},
-
+		
+		increaseVolume: function(){
+			var slider = $("#volumeRange");
+			var newVolume = parseInt(slider.val()) + 1;
+			slider[0].setAttribute("value", newVolume || slider.attributes("min"));
+			this.updateVolume();
+		},
+		
+		decreaseVolume: function(){
+			var slider = $("#volumeRange");
+			var newVolume = parseInt(slider.val()) - 1;
+			slider[0].setAttribute("value", newVolume || slider.attributes("min"));
+			this.updateVolume();
+		},
+		
 		updateVolume: function () {
 			var postiveVolume = $('#volumeRange').val();
 			var displayVolume = 80 - parseInt(postiveVolume, 10);
 
 			this.$el.find('#volumeValue').html("-" + displayVolume + ".0db");
+			this.sendAjax({url: '/denon/volume', value: $('#volumeRange').val()});
 		},
 		
 		updateInput: function () {
 			var selectedInput = $('#selectedInput').val();
 
 			this.$el.find('#inputValue').html(selectedInput);
+			this.sendAjax({url: '/denon/input', value: $('#selectedInput').val()});
 		},
+		
+		sendAjax: function(opts){
+		
+			$.ajax({
+					url:  opts.url,
+					type: 'put',
+					data: JSON.stringify({ value: opts.value })
+				})
+		}
 		
 
 	});
